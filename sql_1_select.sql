a2de367cabf0d99a589291c18f392a049dd872f9
bigom2199 <138991838+bigom2199@users.noreply.github.com>
2023-09-21 10:40:41 +0200

    Update sql_1_select.sql

--- sql_1_select.sql
+++ sql_1_select.sql
@@ -20,70 +20,70 @@ SELECT "hello world" as message , 123 as id;
 # 2: Get everything from the city table
 # (Hint: Many many rows)
 
-SELECT * FROM city ;
+SELECT * FROM `city` ;
 
  
 #
 # 3: Get everything on the cities whose district is "aceh"
 # (Hint: 2 rows)
-  select * FROM city  WHERE 'District' = "aceh";
+  select * FROM `city`  WHERE District = "aceh";
   
   
   
 
 # 4: Get only the name of the cities where the countrycode is "bfa"
-select 'Name' FROM city WHERE 'Countrycode' = "bfa";
+select Name FROM `city` WHERE Countrycode = "bfa";
 
 
 # 5: Get both the name and district of the cities where the countrycode is "tto"
-select * From city;
+select * From `city`;
 
-select 'Name', 'District' FROM city  WHERE 'countryCode' = " tto ";
+select `Name`, District FROM `city`  WHERE `CountryCode` = "tto";
 
 
 # 6: Get the name and district named as nm,dist from the cities where the countrycode is "arm"
-select 'Name' AS nm, 'District' as dist FROM city  WHERE 'CountryCode' = "arm";
+select `Name` AS nm, `District` as dist FROM `city`  WHERE `CountryCode` = "arm";
 
 
 #
 # 7: Get the cities with a name that starts with "bor"
-SELECT *FROM 'city'  WHERE 'Name' LIKE "bor%";
+SELECT *FROM `city`  WHERE `Name` LIKE "bor%";
 
 #
 # 8: Get the cities with a name that contains the string "orto"
-SELECT * FROM 'city' WHERE 'Name' LIKE "%orto%";
+SELECT * FROM `city` WHERE `Name` LIKE "%orto%";
 #
 # 9: Get the cities that has a population below 1000
-SELECT * FROM city  WHERE 'population' < 1000;
+SELECT * FROM `city`  WHERE `Population` < 1000;
 #
 #
 # 10: Get the unique countrycodes from the cities that has a population below 1000
-SELECT DISTINCT('countrycode') FROM city  WHERE 'population' < 1000;
+SELECT DISTINCT(`CountryCode`) FROM `city`  WHERE `Population` < 1000;
 #
 # 11: Get the cities with the countrycode UKR that has more than 1000000 (one million) in population
-SELECT * FROM city WHERE 'countrycode' = "UKR" AND 'population' > 1000000;
+SELECT * FROM `city` WHERE `CountryCode` = "UKR" AND `Population` > 1000000;
 
 #
 
 
 # 12: Get the cities with a population of below 200 or above 9500000 (9.5 million)
-SELECT * FROM city WHERE 'POPULATION' > 9500000 AND 'POpulation' < 200;
+SELECT * FROM `city` WHERE `Population` > 9500000 AND `Population` < 200;
 #
 # 13: Get the cities with the countrycodes TJK, MRT, AND, PNG, SJM
-SELECT * FROM `city` WHERE 'countryCode' IN ("TJK", "MRT", "AND", "PNG", "SJM");
+SELECT * FROM `city` WHERE `CountryCode` IN ("TJK", "MRT", "AND", "PNG", "SJM");
 #
-# 14: Get the cities with a population between 200 and 700 inclusive
-SELECT * FROM `city` WHERE 'POPULATION ' BETWEEN 200 AND 700;
+# 14: Get the cities wiath a population between 200 and 700 inclusive
+SELECT * FROM `city` WHERE `Population` BETWEEN 200 AND 700;
 
 #
 # 15: Get the countries with a population between 8000 and 20000 inclusive
-SELECT * FROM `country` WHERE 'population ' BETWEEN 8000 AND 20000;
+SELECT * FROM `country` WHERE `Population` BETWEEN 8000 AND 20000;
 #
 # 16: Get the name of the countries with a independence year (indepyear) before year 0
-SELECT * FROM `country` WHERE 'IndepYear' < 0;
+SELECT * FROM `country` WHERE `IndepYear` < 0;
 #
 # 17: Get the countries that has no recorded independence year and a population above 1000000
-SELECT * FROM `country` WHERE ' IndepYear' IS NULL AND 'population' > 1000000;
+SELECT * FROM `country` WHERE `IndepYear` IS NULL AND `Population` > 1000000;
 #
 # 18: Get countries with a SurfaceArea below 10 and a defined LifeExpectancy
-SELECT * FROM `country` WHERE 'SurfaceArea' < 10 AND 'LifeExpectancy' IS NOT NULL;
+SELECT * FROM `country` WHERE `SurfaceArea` < 10 AND `LifeExpectancy` IS NOT NULL;
